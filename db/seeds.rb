ab5b85e38a7a36c6bda91315298105a8dd131751
RagtagTurtle <e.russell1792@gmail.com>
2018-05-08 18:52:32 -0500

    created products in seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,32 @@
+product = Product.new(name: "Audi R8", price: 165975, image_url: "https://i.ebayimg.com/00/s/NDgwWDY0MA==/z/n3QAAOSwU91ajD-7/$_20.JPG?set_id=8800005007", description: "The new Audi R8 Coupe shares many similarities with the Audi R8 GT3 LMS. Teams from the motorsports and production worked hand-in-hand, sharing just under 50% of the same parts as the R8 GT3 LMS race car. The main attraction of the R8 is the 5.2-liter V10 FSI® engine, which produces 532 hp of power and 398 lb-ft of torque. Taking it up a notch, the R8 V10 plus cranks out 602 hp and 413 lb-ft of torque. ")
+product.save
+
+product = Product.new(name: "Acura NSX", price: 156000, image_url: "https://media.wheelscene.com/wp-content/uploads/2016/05/First-2017-Acura-NSX-Delivered-880x503.jpg", description: "Draped in colors inspired by the spirit of racing and the subtleties of nature, the NSX is a work of precision-crafted art. The vivid, high-chroma finish emphasizes the car’s sculpted lines for a show-stopping look that turns heads whether it’s shredding the track or standing still.")
+product.save
+
+product = Product.new(name: "Lamborghini Aventador", price: 417650, image_url: "https://1-photos7.motorcar.com/new-2018-lamborghini-aventador-showcar-8868-16805712-3-1024.jpg", description: "Brutally powerful and obscenely flamboyant, the Aventador is unburdened by reality. Crazy expensive, it’s capable of amazing performance without feeling as if it’s going to spin out into a ditch, which is refreshing in a supercar. Available as an S coupe or S roadster, it has a 730-hp 6.5-liter V-12, a seven-speed automated-manual transmission, and all-wheel drive. To help put all that power to pavement, each Aventador has monstrous tires, rear-wheel steering, and adaptive suspension.")
+product.save
+
+product = Product.new(name: "Ferrari 488 GTB", price: 256550, image_url: "https://www.caranddriver.com/images/15q1/654924/2016-ferrari-488gtb-photos-and-info-news-car-and-driver-photo-656010-s-original.jpg", description: "The 488 GTB name marks a return to the classic Ferrari model designation with the 488 in its moniker indicating the engine's unitary displacement, while the GTB stands for Gran Turismo Berlinetta. The new car not only delivers unparalleled performance, it also makes that extreme power exploitable and controllable to an unprecedented level even by less expert drivers.")
+product.save
+
+product = Product.new(name: "McLaren 720S", price: 285000, image_url: "https://s1.cdn.autoevolution.com/images/news/mclaren-720s-lowered-on-novitec-mc1-wheels-looks-like-a-racecar-121389_1.jpg", description: "The McLaren 720S embodies our relentless quest to push the limits of possibility. Lighter, stronger, faster. It’s all of these and more. But how it makes you feel is altogether something else. Prepare to push the limits of what you thought possible in a supercar.")
+product.save
+
+product = Product.new(name: "Koenigsegg One 1", price: 2850000, image_url: "http://www.thesupercarblog.com/wp-content/uploads/2016/08/2015-Koenigsegg-One-1-for-sale-in-USA-10.jpg", description: "The One:1 was introduced in 2014. Seven examples, including one prototype, were built during 2014 and 2015.
+
+This was one of the most exclusive production car programs ever envisaged in the car industry.
+
+The hp-to-kg curb weight ratio is an astonishing 1:1. This is the “dream” equation, previously thought impossible when it comes to fully road legal and usable sports cars. The One:1 is the first homologated production car in the world with one Megawatt of power, thereby making it the world´s first series produced Megacar.
+
+Thus, the name One:1.
+
+")
+product.save
+
+product = Product.new(name: "Ford GT", price: 450000, image_url: "https://www.ford.com/campaignlibs/content/dam/ford_com/gtreveal/config_images/FordGT_Config_1440px.jpg", description: "Innovation. That’s what the Ford GT is all about. From its aerodynamically optimized shape to its multifunctional buttresses to its extraordinarily powerful 3.5L EcoBoost® V6 engine, everything about the Ford GT is designed to deliver pure performance.")
+product.save
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
