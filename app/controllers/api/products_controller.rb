d02470dcdd9ee5f1afad86327bd0393eaa9b023f
RagtagTurtle <e.russell1792@gmail.com>
2018-05-08 19:28:16 -0500

    added views

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,4 +4,39 @@ class Api::ProductsController < ApplicationController
     render 'all_products.json.jbuilder'
   end
 
+  def product_1
+    @product = Product.find(1)
+    render 'first_product_view.json.jbuilder'
+  end
+
+  def product_2
+    @product = Product.find(2)
+    render 'second_product_view.json.jbuilder'
+  end
+
+  def product_3
+    @product = Product.find(3)
+    render 'product_3_view.json.jbuilder'
+  end
+
+  def product_4
+    @product = Product.find(4)
+    render 'product_4_view.json.jbuilder'
+  end
+
+  def product_5
+    @product = Product.find(5)
+    render 'product_5_view.json.jbuilder'
+  end
+
+  def product_6
+    @product = Product.find(6)
+    render 'product_6_view.json.jbuilder'
+  end
+
+  def product_7
+    @product = Product.find(7)
+    render 'product_7_view.json.jbuilder'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/all_products' => 'products#all_products'
+    get '/product_1' => 'products#product_1'
+    get '/product_2' => 'products#product_2'
+    get '/product_3' => 'products#product_3'
+    get '/product_4' => 'products#product_4'
+    get '/product_5' => 'products#product_5'
+    get '/product_6' => 'products#product_6'
+    get '/product_7' => 'products#product_7'
   end
 end
